1a1870607ef714cb753f54f9c97ba581d0c2f392
Hugh Saunders <hugh@wherenow.org>
2011-06-13 20:43:49 +0100

    xml stripped out of backup files and parsed

---
+++ tumblr-restore.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import re
+from lxml import etree
+from lxml import objectify
+
+class BackupParser:
+	def __init__(self,backup_dir):
+		self.backup_dir=backup_dir
+		if not os.path.exists(backup_dir+"/index.html"):
+			raise ValueError("Specified Backup Directory Doesn't Exist")
+		self.posts_dir=backup_dir+"/posts"
+
+	def extract_xml_string(self,filename):
+		file = open(filename,'r')
+		lines=file.readlines()
+		begin_line=0
+		end_line=0
+		begin_re=re.compile("<\?xml version=.* encoding=\"UTF-8\"")
+		end_re=re.compile("END TUMBLR XML")
+		for i in range(0,len(lines)):
+			line=lines[i]
+			if begin_re.search(line):
+				begin_line=i+1
+				continue
+			if end_re.search(line):
+				end_line=i
+				break
+		lines[begin_line]=lines[begin_line].lstrip()
+		return "\n".join(lines[begin_line:end_line])
+		
+	def parse(self):
+		for filename in os.listdir(self.posts_dir):
+			xml_string=self.extract_xml_string(self.posts_dir+"/"+filename)
+			post=etree.fromstring(xml_string)
+			print post.get("id"),post.get("type")
+			for element in post:
+				print "",element.tag
+			
+
+
+
+if __name__=="__main__":
+	bp=BackupParser(sys.argv[1])
+	bp.parse()
+
